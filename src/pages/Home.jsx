26b4d17c71b27c97a4154acdb78fe0d9a61cb14c
EugeneSince1993 <jk235@mail.ru>
2022-04-07 01:01:12 +0300

    l7 55.30

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,6 +26,7 @@ function Home({ items }) {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {
+          items &&
           items.map((obj) => (
             <PizzaBlock key={obj.id} {...obj} />
           ))
